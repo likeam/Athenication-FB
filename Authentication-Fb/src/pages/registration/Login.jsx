10474dc5801181212098e88c3f5fbfc73b0159ec
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-01-28 23:05:51 +0500

    Building connection to Login & Signin FB

--- Authentication-Fb/src/pages/registration/Login.jsx
+++ Authentication-Fb/src/pages/registration/Login.jsx
@@ -1,7 +1,33 @@
-import { Link } from "react-router-dom"
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { Link, useNavigate } from "react-router-dom"
+import { auth } from "../../firebase/FirebaseConfig";
+import  {useState} from "react";
 
 
 function Login() {
+
+    
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    const login = async() => {
+        if(email === '' || password === ''){
+            return alert("Please  fill the all fields.")
+        }
+        try{
+            const user = await signInWithEmailAndPassword(auth, email, password);
+            const users = localStorage.setItem('user', JSON.stringify(user));
+            alert("Login Sucessful.");
+            navigate('/');
+            setEmail('');
+            setPassword('');
+        }catch(error){
+            console.error(error);
+        }
+    }
+
    
     return (
         <div className=' flex justify-center items-center h-screen'>
@@ -12,6 +38,8 @@ function Login() {
                 <div>
                     <input type="email"
                         name='email'
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
                         className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Email'
                     />
@@ -19,12 +47,15 @@ function Login() {
                 <div>
                     <input
                         type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
                         className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Password'
                     />
                 </div>
                 <div className=' flex justify-center mb-3'>
                     <button
+                        onClick={login}
                         className=' bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg'>
                         Login
                     </button>
--- Authentication-Fb/src/pages/registration/Signup.jsx
+++ Authentication-Fb/src/pages/registration/Signup.jsx
@@ -1,7 +1,27 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { auth } from '../../firebase/FirebaseConfig';
 
 function Signup() {
    
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signup = async() => {
+        if(email === '' || password === ''){
+            return alert("Please  fill the all fields.")
+        }
+        try{
+            const user = await createUserWithEmailAndPassword(auth, email, password);
+            alert("Signin Sucessful.");
+            setEmail('');
+            setPassword('');
+        }catch(error){
+            console.error(error);
+        }
+    }
+
     return (
         <div className=' flex justify-center items-center h-screen'>
             <div className=' bg-gray-800 px-10 py-10 rounded-xl '>
@@ -11,6 +31,8 @@ function Signup() {
                 <div>
                     <input type="email"
                         name='email'
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
                         className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Email'
                     />
@@ -18,12 +40,15 @@ function Signup() {
                 <div>
                     <input
                         type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
                         className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Password'
                     />
                 </div>
                 <div className=' flex justify-center mb-3'>
                     <button
+                        onClick={signup}
                         className=' bg-red-500 w-full text-white font-bold  px-2 py-2 rounded-lg'>
                         Signup
                     </button>
