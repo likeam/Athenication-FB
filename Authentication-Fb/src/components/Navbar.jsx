4d3f72dc288dc89d52a3738a51cea1937d2d4811
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-01-29 22:53:56 +0500

    Building Logot

--- Authentication-Fb/src/components/Navbar.jsx
+++ Authentication-Fb/src/components/Navbar.jsx
@@ -1,24 +1,39 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 function Navbar() {
+
+    const user = JSON.parse(localStorage.getItem('user'));
+    
+    const navigate = useNavigate();
+
+    const logout = () => {
+        localStorage.clear('user');
+        alert("Logout Sucessfully");
+        navigate('/');
+    }
+
+
     return (
         <div className='main lg:flex md:flex flex-wrap justify-between items-center 
      px-4 bg-[#2a056f] py-4 shadow-md'>
             <div className="left">
-                <div className="logo font-bold text-2xl text-white text-center">E-NoteBook</div>
+                <div className="text-2xl font-bold text-center text-white logo">E-NoteBook</div>
             </div>
             <div className="right">
-                <ul className='flex space-x-4 text-white justify-center items-center'>
+                <ul className='flex items-center justify-center space-x-4 text-white'>
                     <Link to={'/'}>
                         <li className='cursor-pointer'>Home</li>
                     </Link>
-                    <Link to={'/login'}>
+                    {!user && <Link to={'/signin'}>
+                        <li className='cursor-pointer'>Signin</li>
+                    </Link>}
+                    
+                    {!user && <Link to={'/login'}>
                         <li className='cursor-pointer'>Login</li>
-                    </Link>
-                    <Link to={'/signup'}>
-                        <li className='cursor-pointer'>Signup</li>
-                    </Link>
+                    </Link>}
+                    {user && <li onClick={logout} className='cursor-pointer'>Logout</li> }
+                    {user && <li  className='cursor-pointer'>Welcome : {user.user.email}</li> }
                 </ul>
             </div>
         </div>
